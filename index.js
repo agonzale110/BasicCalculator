7523fa757f0b88b52b841b0467ad40a45d4580f8
agonzale110 <agonzale110@cuc.edu.co>
2022-09-17 17:01:35 -0500

    Primer commit, calculadora grafica funcional

---
+++ index.js
@@ -0,0 +1,91 @@
+$(document).ready(function()    {
+    $('#miCalculadora').modal('show');
+    $('#miCalculadora').on('hide.bs.modal', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    });
+
+    let primerNumero = '0', segundoNumero = '0';
+    let opSeleccionado;
+
+    const hacerOperacion = {
+        '+': function (x, y) { return (+x.replace(',', '.')) + (+y.replace(',', '.')) },
+        '-': function (x, y) { return (+x.replace(',', '.')) - (+y.replace(',', '.')) },
+        'x': function (x, y) { return (+x.replace(',', '.')) * (+y.replace(',', '.')) },
+        '÷': function (x, y) { return (+x.replace(',', '.')) / (+y.replace(',', '.')) },
+    };
+    
+    const cambiarSigno = (numero) => {
+        if (numero.includes('-')) {
+            return numero.replace('-', '');
+        } else {
+            return '-'+numero;
+        }
+    }
+
+    const updateResultado = (valor) => {
+        $('#resultado').attr("title",valor);
+        $('#resultado').html(valor);
+    }
+
+    $( "button" ).click(function(e) {
+        if (e.target.innerText == ',') {
+            if (!opSeleccionado) {
+                primerNumero = primerNumero+',';
+                updateResultado(primerNumero);
+            } else {
+                segundoNumero = segundoNumero+',';
+                updateResultado(segundoNumero);
+            }
+        } else if (e.target.innerText == '+/-') {
+            if (!opSeleccionado) {
+                if (primerNumero == '0')
+                    return
+
+                primerNumero = cambiarSigno(primerNumero);
+                updateResultado(primerNumero);
+            } else {
+                if (segundoNumero == '0')
+                    return
+
+                segundoNumero = cambiarSigno(segundoNumero);
+                updateResultado(segundoNumero);
+            }
+        }   else if (e.target.innerText == 'C') {
+            if (!opSeleccionado) {
+                primerNumero = '0';
+                updateResultado(primerNumero);
+            } else {
+                segundoNumero = '0';
+                updateResultado(segundoNumero);
+            }
+        } else if (isNaN(e.target.innerText) && e.target.innerText != '=' && e.target.innerText != ',' && e.target.innerText != '+/-' && e.target.innerText != 'C') {
+            if (e.target.innerText == 'AC') {
+                primerNumero = '0';
+                segundoNumero = '0';
+                opSeleccionado = undefined;
+                updateResultado(primerNumero);
+            } else {
+                opSeleccionado = e.target.innerText;
+                updateResultado('0');
+            }
+        } else {
+            if (primerNumero == '0') {
+                primerNumero = e.target.innerText;
+                updateResultado(primerNumero);
+            } else if (opSeleccionado == undefined && e.target.innerText != '=') {
+                primerNumero = primerNumero + (+e.target.innerText);
+                updateResultado(primerNumero);
+            } else if (e.target.innerText == '=') {
+                primerNumero = (opSeleccionado ? String(hacerOperacion[opSeleccionado](primerNumero, segundoNumero)).replace('.', ',') : primerNumero);
+                segundoNumero = '0';
+                opSeleccionado = undefined;
+                updateResultado(primerNumero);
+            } else if (!isNaN(e.target.innerText)) {
+                segundoNumero = (segundoNumero == '0' ? e.target.innerText : segundoNumero + (+e.target.innerText));
+                updateResultado(segundoNumero);
+            }
+        }
+    });
+});
